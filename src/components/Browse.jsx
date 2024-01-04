59862c8601abc32610f0b39061fdbdfffc273b88
SharathSana3245 <sharathkumarbfbf@gmail.com>
2024-01-04 15:17:02 +0530

    adding files

---
+++ src/components/Browse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Browse(props) {
+    
+
+    return (
+        <div className="text-3xl font-bold text-green-800">
+            Browsesscdv
+        </div>
+    )
+}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Header } from './Header'
 
 export const Login = () => {
-    return (
+    const [isSignInForm,setIsSignInForm]=useState(true)
+    const handleSignInForm=()=>{
+        setIsSignInForm(!isSignInForm)
+    }
+return(
         <div>
-            
+            <Header/>
+            <img
+             className='absolute'
+             src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="logo"/>
+             <form className='w-3/12 absolute p-6 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 bg-black'>
+                <h1 className='font-bold text-3xl py-4'>{isSignInForm ?"SignIn":"SignUp"}</h1>
+                {!isSignInForm &&<input type="text" placeholder="Full Name" className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>}
+                <input type="text" placeholder="Email" className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>
+                <input type="password" placeholder="Password" className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>
+                <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>
+                {isSignInForm ?"SignIn":"SignUp"}
+                </button>
+                <p className='py-4 cursor-pointer' onClick={handleSignInForm}>{isSignInForm ?"New to Netflix SignUp Now? ":"Already registered SignIn now"}</p>
+             </form>
         </div>
     )
 }
